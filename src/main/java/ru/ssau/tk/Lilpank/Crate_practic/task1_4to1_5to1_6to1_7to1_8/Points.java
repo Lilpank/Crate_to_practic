a6deda9f03ed5cced66a726c41d8c91f29c11e28
Lilpank <2019-00464@students.ssau.ru>
2020-09-14 12:45:18 +0400

    1. correct Points.

--- src/main/java/ru/ssau/tk/Lilpank/Crate_practic/task1_4to1_5to1_6to1_7to1_8/Points.java
+++ src/main/java/ru/ssau/tk/Lilpank/Crate_practic/task1_4to1_5to1_6to1_7to1_8/Points.java
@@ -8,43 +8,43 @@ public class Points {
 
     }
 
-    public static Point sum(Point obj1, Point obj2, Point obj3) {
-        return obj3 = new Point(obj1.getX() + obj2.getX(), obj1.getY() + obj2.getY(), obj1.getZ() + obj2.getZ());
+    public static Point sum(Point a, Point b) {
+        return new Point(a.getX() + b.getX(), a.getY() + b.getY(), a.getZ() + b.getZ());
     }
 
-    public static Point subtract(Point obj1, Point obj2, Point obj3) {
-        return obj3 = new Point(obj1.getX() - obj2.getX(), obj1.getY() - obj2.getY(), obj1.getZ() - obj2.getZ());
+    public static Point subtract(Point a, Point b) {
+        return new Point(a.getX() - b.getX(), a.getY() - b.getY(), a.getZ() - b.getZ());
     }
 
-    public static Point multiply(Point obj1, Point obj2, Point obj3) {
-        return obj3 = new Point(obj1.getX() * obj2.getX(), obj1.getY() * obj2.getY(), obj1.getZ() * obj2.getZ());
+    public static Point multiply(Point a, Point b) {
+        return new Point(a.getX() * b.getX(), a.getY() * b.getY(), a.getZ() * b.getZ());
     }
 
-    public static Point devide(Point obj1, Point obj2, Point obj3) {
-        return obj3 = new Point(obj1.getX() / obj2.getX(), obj1.getY() / obj2.getY(), obj1.getZ() / obj2.getZ());
+    public static Point devide(Point a, Point b) {
+        return new Point(a.getX() / b.getX(), a.getY() / b.getY(), a.getZ() / b.getZ());
     }
 
-    public static Point enlarge(Point obj, double alfa) {
-        return new Point(obj.getX() * alfa, obj.getY() * alfa, obj.getZ() * alfa);
+    public static Point enlarge(Point a, double alfa) {
+        return new Point(a.getX() * alfa, a.getY() * alfa, a.getZ() * alfa);
     }
 
-    public static double length(Point obj) {
-        return obj.length();
+    public static double length(Point a) {
+        return a.length();
     }
 
-    public static Point opposite(Point obj) {
-        return new Point(-obj.getX(), -obj.getY(), -obj.getZ());
+    public static Point opposite(Point a) {
+        return new Point(-a.getX(), -a.getY(), -a.getZ());
     }
 
-    public static Point inverse(Point obj) {
-        return new Point(1 / obj.getX(), 1 / obj.getY(), 1 / obj.getZ());
+    public static Point inverse(Point a) {
+        return new Point(1 / a.getX(), 1 / a.getY(), 1 / a.getZ());
     }
 
-    public static double scalarProduct(Point obj1, Point obj2) {
-        return obj1.getX() * obj2.getX() + obj1.getY() * obj2.getY() + obj1.getZ() * obj2.getZ();
+    public static double scalarProduct(Point a, Point b) {
+        return a.getX() * b.getX() + a.getY() * b.getY() + a.getZ() * b.getZ();
     }
 
-    public static Point vectorProduct(Point obj1, Point obj2) {
-        return new Point(obj1.getY() * obj2.getZ() - obj1.getZ() * obj2.getY(), obj1.getZ() * obj2.getX() - obj1.getX() * obj2.getZ(), obj1.getX() * obj2.getY() - obj1.getY() * obj2.getX());
+    public static Point vectorProduct(Point a, Point b) {
+        return new Point(a.getY() * b.getZ() - a.getZ() * b.getY(), a.getZ() * b.getX() - a.getX() * b.getZ(), a.getX() * b.getY() - a.getY() * b.getX());
     }
 }
