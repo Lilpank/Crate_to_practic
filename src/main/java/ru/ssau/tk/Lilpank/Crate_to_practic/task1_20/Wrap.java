7b3c824442037332e5e4d61df6272fa62da791f9
Даниил <2019-00464@students.ssau.ru>
2020-09-19 19:35:18 +0300

    Создал класс где реализовано:упаковка/распаковка типов, автоупаковка и автораспаковка

---
+++ src/main/java/ru/ssau/tk/Lilpank/Crate_to_practic/task1_20/Wrap.java
@@ -0,0 +1,37 @@
+package ru.ssau.tk.Lilpank.Crate_to_practic.task1_20;
+
+public class Wrap {
+    @Deprecated
+    static Integer boxing(int i) {
+        return new Integer(i);
+    }
+
+    @Deprecated
+    static Boolean boxing(boolean b) {
+        return new Boolean(b);
+    }
+
+    static Boolean autoBoxing(boolean i) {
+        return i;
+    }
+
+    static Integer autoBoxing(int i) {
+        return i;
+    }
+
+    static int unboxing(Integer i) {
+        return i.intValue();
+    }
+
+    static boolean unboxing(Boolean b) {
+        return b.booleanValue();
+    }
+
+    static boolean autoUnboxing(Boolean b) {
+        return b;
+    }
+
+    static int autoUnboxing(Integer i) {
+        return i;
+    }
+}
