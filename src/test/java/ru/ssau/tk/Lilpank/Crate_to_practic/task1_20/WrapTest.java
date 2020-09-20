5101b945503945f56a74df83a80da0ab938d6d9b
Даниил <2019-00464@students.ssau.ru>
2020-09-20 09:30:09 +0300

    Добавил assertEquals

--- src/test/java/ru/ssau/tk/Lilpank/Crate_to_practic/task1_20/WrapTest.java
+++ src/test/java/ru/ssau/tk/Lilpank/Crate_to_practic/task1_20/WrapTest.java
@@ -6,16 +6,17 @@ import sun.font.TrueTypeFont;
 import static org.testng.Assert.*;
 
 public class WrapTest {
+    int i = 5;
+    int j = 3;
+    boolean b = true;
+    Integer I = Wrap.boxing(i);
+    Boolean conditionTrue = Wrap.boxing(true);
+    Boolean conditionFalse = Wrap.autoBoxing(false);
+    Boolean conditionNull = null;
+
     @Test
-    public void test() {
-        int i = 5;
-        int j = 3;
-        boolean b = true;
-        Integer I = Wrap.boxing(i);
-        Boolean conditionTrue = Wrap.boxing(true);
-        Boolean conditionFalse = Wrap.autoBoxing(false);
-        Boolean conditionNull = null;
-        if (conditionTrue){
+    public void testIf() {
+        if (conditionTrue) {
             System.out.println("Этот блок кода будет работать");
         }
         if (Boolean.TRUE.equals(conditionNull)) {
@@ -23,6 +24,18 @@ public class WrapTest {
         }
         if (Boolean.TRUE.equals(conditionFalse)) {
             System.out.println("Этот блок кода не будет работать");
+        }
+
+    }
+
+
+    @Test
+    public void testBox() {
+        if (conditionTrue) {
+            assertEquals(Wrap.autoBoxing(i), i, 0.0001);
+            assertEquals((Wrap.autoBoxing(++I)), ++i, 0.0001);
+            assertEquals(Wrap.autoUnboxing(++I), ++i, 0.0001);
+            assertEquals(Wrap.autoUnboxing(i), i, 0.0001);
         }
     }
 }
