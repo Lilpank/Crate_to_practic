8140116c9f1b175f98047a5d7595474d7b129e6c
Даниил <2019-00464@students.ssau.ru>
2020-09-21 11:46:21 +0300

    completed task2_3

---
+++ src/test/java/ru/ssau/tk/Lilpank/Crate_to_practic/task2_3/task2_3Test.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.Lilpank.Crate_to_practic.task2_3;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class task2_3Test {
+    @Test
+    public void testSetNumber() {
+        int[] arr = task2_3.setNumber(5);
+        assertEquals(arr[4],9);
+        assertEquals(arr.length,5);
+    }
+}
