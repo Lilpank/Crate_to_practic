5df20d624e7e93f13b3d71dc87e26c9eeb5dda21
Даниил <2019-00464@students.ssau.ru>
2020-09-23 10:42:27 +0300

    completed task 2_14

---
+++ src/main/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task2_14/Task2_14.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.Lilpank.Crate_to_practic.Task2_14;
+
+
+public class Task2_14 {
+    static void setArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = -((Integer) array[i]);
+
+        }
+    }
+}
+
