4af677e55735520739064763eea7837db914cb06
Даниил <2019-00464@students.ssau.ru>
2020-09-24 19:58:47 +0300

    completed task 2_20

--- src/main/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task2_20/Task2_20.java
+++ src/main/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task2_20/Task2_20.java
@@ -2,15 +2,14 @@ package ru.ssau.tk.Lilpank.Crate_to_practic.Task2_20;
 
 public class Task2_20 {
     static boolean setArray(int[] array) {
-        int size = array.length;
         int _1count = 0;
         int _2count = 0;
-        for (int k=0;k< array.length;k++) {
-            if (array[k] % array[0] == 0)
+        for (int j : array) {
+            if (j % array[0] == 0)
                 _1count++;
         }
-        for (int j =0;j< array.length;j++) {
-            if (array[j] % array[size-1] == 0)
+        for (int i : array) {
+            if (i % array[array.length - 1] == 0)
                 _2count++;
         }
         return _1count > _2count;
