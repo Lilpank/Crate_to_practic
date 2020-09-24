9c2c778cc4f77dc596db8db939215259e748f68f
Даниил <2019-00464@students.ssau.ru>
2020-09-24 11:20:32 +0300

    completed task 2_19

---
+++ src/main/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task2_19/Task2_19.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk.Lilpank.Crate_to_practic.Task2_19;
+
+public class Task2_19 {
+    static int getArrayEven(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                sum = array[i];
+            }
+        }
+
+        return sum;
+
+    }
+}
---
+++ src/test/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task2_19/Task2_19Test.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.Lilpank.Crate_to_practic.Task2_19;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Task2_19Test {
+
+    @Test
+    public void testGetArrayEven() {
+    }
+}
