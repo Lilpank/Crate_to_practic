5eac99632289974aaec7581579330f60000f957e
Даниил <2019-00464@students.ssau.ru>
2020-09-24 11:20:22 +0300

    completed task 2_19

--- src/test/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task2_18/Task2_18Test.java
+++ src/test/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task2_18/Task2_18Test.java
@@ -9,7 +9,7 @@ public class Task2_18Test {
     @Test
     public void testMaxvalue() {
         int[] array = new int[]{121, 43, 81, 22, 6};
-        assertNull(Task2_18.Maxvalue(new int[]{}),null);
+        assertNull(Task2_18.Maxvalue(new int[]{}), null);
 
     }
 }
