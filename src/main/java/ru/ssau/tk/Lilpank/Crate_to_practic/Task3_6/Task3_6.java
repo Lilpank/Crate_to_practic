2a0d3360b41741d415c5fba369dd17a17ac80c5d
Даниил <2019-00464@students.ssau.ru>
2020-09-28 11:13:21 +0300

    escape-последовательностей java для использования в строках.
    \t  — Символ табуляции (в java – эквивалент четырех пробелов);
    \b — Символ возврата в тексте на один шаг назад или удаление одного символа в строке (backspace);
    \n — Символ перехода на новую строку;
    \r — Символ возврата каретки;
    \f— Прогон страницы к началу следующей страницы;
    \’ — Символ одинарной кавычки;
    \” — Символ двойной кавычки;
    \\ — Символ обратной косой черты (\).

---
+++ src/main/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task3_6/Task3_6.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.Lilpank.Crate_to_practic.Task3_6;
+
+public class Task3_6 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Символ\t№" + ++i);
+        System.out.println("Символ\b№" + ++i);
+        System.out.println("Символ\n№" + ++i);
+        System.out.println("Символ\r№" + ++i);
+        System.out.println("Символ\'№" + ++i);
+        System.out.println("Символ\"№" + ++i);
+        System.out.println("Символ\\№" + ++i);
+
+    }
+
+}
