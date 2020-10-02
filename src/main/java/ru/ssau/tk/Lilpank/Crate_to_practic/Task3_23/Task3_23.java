cf7da35580a9f6a27774436cd288c3c209647392
Даниил <2019-00464@students.ssau.ru>
2020-10-02 19:58:01 +0300

    Completed Task 3_23

---
+++ src/main/java/ru/ssau/tk/Lilpank/Crate_to_practic/Task3_23/Task3_23.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.Lilpank.Crate_to_practic.Task3_23;
+
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
+
+import java.nio.charset.Charset;
+
+public class Task3_23 {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset());
+    }
+}
