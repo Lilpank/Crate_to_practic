91f4a11df16debb6d0d1605f6a10e81bd1ea3d9e
Даниил <2019-00464@students.ssau.ru>
2020-10-20 10:45:06 +0300

    Смог получить данные с сайта и вывел их в консоль.

--- src/main/java/ru/ssau/tk/Lilpank/WeatherZhigulevsk/Parser.java
+++ src/main/java/ru/ssau/tk/Lilpank/WeatherZhigulevsk/Parser.java
@@ -3,6 +3,8 @@ package ru.ssau.tk.Lilpank.WeatherZhigulevsk;
 import org.jsoup.Jsoup;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,6 +17,21 @@ public class Parser {
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(getPage());
+        Document page = getPage();
+        String date;
+
+        Element divCountry = page.select("div.header-title.header-title_in-fact").first();
+        Element nameCountry = divCountry.select("h1.title.title_level_1.header-title__title").first();
+
+        Element divTheme = page.select("div.fact__time-yesterday-wrap").first();
+        Elements valueTheme = divTheme.select("time.time.fact__time");
+
+
+        Element divTemp = page.select("div.temp.fact__temp.fact__temp_size_s").first();
+        Elements valueTemp = divTemp.select("span.temp__value");
+
+        System.out.println(valueTheme.text());
+        System.out.println(nameCountry.text() + " " + valueTemp.text());
+
     }
 }
