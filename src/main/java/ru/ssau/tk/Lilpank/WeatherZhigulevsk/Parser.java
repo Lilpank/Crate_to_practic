acb72a78790a061fbc2887793c8d123d5c499434
Даниил <2019-00464@students.ssau.ru>
2020-10-20 12:23:37 +0300

    Иногда появляется ошибка. Сделал шаблончик try catch

--- src/main/java/ru/ssau/tk/Lilpank/WeatherZhigulevsk/Parser.java
+++ src/main/java/ru/ssau/tk/Lilpank/WeatherZhigulevsk/Parser.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 
 public class Parser {
@@ -17,21 +18,24 @@ public class Parser {
     }
 
     public static void main(String[] args) throws IOException {
-        Document page = getPage();
-        String date;
+        try {
+            Document page = getPage();
+            String date;
 
-        Element divCountry = page.select("div.header-title.header-title_in-fact").first();
-        Element nameCountry = divCountry.select("h1.title.title_level_1.header-title__title").first();
+            Element divCountry = page.select("div.header-title.header-title_in-fact").first();
+            Element nameCountry = divCountry.select("h1.title.title_level_1.header-title__title").first();
 
-        Element divTheme = page.select("div.fact__time-yesterday-wrap").first();
-        Elements valueTheme = divTheme.select("time.time.fact__time");
+            Element divTheme = page.select("div.fact__time-yesterday-wrap").first();
+            Elements valueTheme = divTheme.select("time.time.fact__time");
 
 
-        Element divTemp = page.select("div.temp.fact__temp.fact__temp_size_s").first();
-        Elements valueTemp = divTemp.select("span.temp__value");
+            Element divTemp = page.select("div.temp.fact__temp.fact__temp_size_s").first();
+            Elements valueTemp = divTemp.select("span.temp__value");
 
-        System.out.println(valueTheme.text());
-        System.out.println(nameCountry.text() + " " + valueTemp.text());
-
+            System.out.println(valueTheme.text());
+            System.out.println(nameCountry.text() + " " + valueTemp.text());
+        } catch (SocketTimeoutException e) {
+            System.out.println("Упс, произошла ошибка.Запустите программу ещё разок.");
+        }
     }
 }
