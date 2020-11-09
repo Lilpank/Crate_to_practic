fdf90d1e9d3db245475bd29974886e3bd752489c
Даниил <2019-00464@students.ssau.ru>
2020-11-09 16:12:14 +0300

    Танчик полностью перемещается

--- src/main/java/ru/ssau/tk/Lilpank/MyProjects/GameShooter/KeyInput.java
+++ src/main/java/ru/ssau/tk/Lilpank/MyProjects/GameShooter/KeyInput.java
@@ -17,13 +17,41 @@ public class KeyInput extends KeyAdapter {
             GameObject tempObject = handler.object.get(i);
             if (tempObject.getId() == ID.Player) {
                 if (key == KeyEvent.VK_LEFT) {
-                    tempObject.setX(tempObject.getX() - 2);
+                    tempObject.setVelX(-5);
                 }
+                if (key == KeyEvent.VK_UP) {
+                    tempObject.setVelY(-5);
+                }
+                if (key == KeyEvent.VK_DOWN) {
+                    tempObject.setVelY(5);
+                }
+                if (key == KeyEvent.VK_RIGHT) {
+                    tempObject.setVelX(5);
+                }
+
             }
         }
     }
 
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+            if (tempObject.getId() == ID.Player) {
+                if (key == KeyEvent.VK_LEFT) {
+                    tempObject.setVelX(0);
+                }
+                if (key == KeyEvent.VK_UP) {
+                    tempObject.setVelY(0);
+                }
+                if (key == KeyEvent.VK_DOWN) {
+                    tempObject.setVelY(0);
+                }
+                if (key == KeyEvent.VK_RIGHT) {
+                    tempObject.setVelX(0);
+                }
+
+            }
+        }
     }
 }
